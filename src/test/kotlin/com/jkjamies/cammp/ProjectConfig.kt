2b34676f8fc91cb66f97807fa938f09d7af50622
jkjamies <jkjamies@mtu.edu>
2025-12-29 21:04:59 -0700

    Configures concurrent test execution
    Enables concurrent spec and test execution in Kotest
    to improve test suite execution time.

---
+++ src/test/kotlin/com/jkjamies/cammp/ProjectConfig.kt
@@ -0,0 +1,10 @@
+package com.jkjamies.cammp
+
+import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.engine.concurrency.SpecExecutionMode
+import io.kotest.engine.concurrency.TestExecutionMode
+
+object ProjectConfig : AbstractProjectConfig() {
+    override val specExecutionMode = SpecExecutionMode.Concurrent
+    override val testExecutionMode = TestExecutionMode.Concurrent
+}
